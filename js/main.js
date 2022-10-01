fd53f1b7337abc22f6e09c370584a7e668f2ba7d
KeltAntares <valerii.deineka.volodymyrovych@gmail.com>
2022-10-01 19:30:23 +0300

    Card transition, fixed theme modes

--- js/main.js
+++ js/main.js
@@ -3,9 +3,50 @@ import LazyLoad from 'vanilla-lazyload';
 
 // Parts
 import { tabs } from './parts/tabs';
-import { themeModes } from './parts/themeModes';
+
+function log() {
+    console.log(window);
+    console.log(localStorage.getItem("lightMode"));
+}
+
+function toggle_theme() {
+    var main_el = document.getElementsByTagName("body")[0];
+    let $button = document.querySelector('#themeMode');
+    $button.addEventListener('click', function(a){
+        if (main_el.classList.contains("lightMode")) {
+            localStorage.setItem("lightMode", 0);
+            main_el.classList.remove('lightMode');
+            $button.classList.remove('themeModeToggle--checked');
+            console.log("Dark");
+        } else {
+            localStorage.setItem("lightMode", 1);
+            main_el.classList.add('lightMode');
+            $button.classList.add('themeModeToggle--checked');
+            console.log("Light");
+        }
+    });
+
+    log();
+}
+
+window.onload = function(){
+    log();
+    var main_el = document.getElementsByTagName("body")[0];
+    let $button = document.querySelector('#themeMode');
+    if (localStorage.getItem("lightMode") == 0) {
+        main_el.classList.remove('lightMode');
+        $button.classList.remove('themeModeToggle--checked');
+        console.log("Dark");
+    } else if (localStorage.getItem("lightMode") == 1) {
+        main_el.classList.add('lightMode');
+        $button.classList.add('themeModeToggle--checked');
+        console.log("Light");
+    } else {
+        console.error(localStorage.getItem("lightMode"));
+    }
+};
 
 tabs();
-themeModes();
+toggle_theme();
 
 new LazyLoad();
